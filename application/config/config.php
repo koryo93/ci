cc61a4ebb5086912e04f668e75e75df9afa13590
koryo93 <hsoo.choi@gmail.com>
2019-01-13 21:09:32 +0900

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci/';
+$config['base_url'] = 'http://localhost/cboard/';
 
 /*
 |--------------------------------------------------------------------------
@@ -77,6 +77,7 @@ $config['url_suffix'] = '';
 |
 */
 $config['language']	= 'korean';
+// $config['language']	= 'english';
 
 /*
 |--------------------------------------------------------------------------
